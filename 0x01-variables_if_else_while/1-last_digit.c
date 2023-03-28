4b65f2e777c5a19da2eaf77df62fc8bd808926b7
sarasinah <mabelane.lsn@gmail.com>
2023-03-28 04:13:46 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	}
 	else if (ld == 0)
 	{
-		  printf("Last digit of %d is %d and is equal to zero\n", n, ld);
+		  printf("Last digit of %d is %d and is equal to 0\n", n, ld);
 	}
 	else if (ld < 6 && ld != 0)
 	{
