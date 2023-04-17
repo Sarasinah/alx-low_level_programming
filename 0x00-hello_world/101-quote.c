060f2231786ac3f05c6f30d995146f10f7fd161d
sarasinah <mabelane.lsn@gmail.com>
2023-04-17 03:49:19 -0700

    main.h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
   * main - prints exactly "and that piece of art is useful"
   * - Dora Korpar, 2015-10-19",
   * followed by a new line, to the standard error.
-  * Return: Always 0 (success)
+  * Return: Always 1 (success)
   */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-29\n", 59);
+	fputs(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-29\n", stdout);
 	return (1);
 }
